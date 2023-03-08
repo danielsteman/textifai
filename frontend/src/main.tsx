865eb63e515ef7b05317143f4c77ba9d6e96337a
Daniel Steman <daniel-steman@live.nl>
2023-03-08 04:01:27 +0000

    added login and signup placeholders

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Root from "./router/root";
+import ErrorPage from "./routes/ErrorPage";
+import Login from "./routes/Login";
+import Root from "./routes/Root";
+import SignUp from "./routes/SignUp";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "signup",
+    element: <SignUp />,
   },
 ]);
 
