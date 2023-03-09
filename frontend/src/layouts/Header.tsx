e46d4c2bf9db90d2e094e8aa768b3727fa2b57ef
Daniel Steman <daniel-steman@live.nl>
2023-03-09 04:52:35 +0000

    added logo

--- frontend/src/layouts/Header.tsx
+++ frontend/src/layouts/Header.tsx
@@ -1,14 +1,20 @@
-import { Button, HStack } from "@chakra-ui/react";
+import { Button, Flex, HStack, Icon, Spacer } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+import Logo from "../components/Logo";
 
 const Header = () => {
   return (
     <>
-      <HStack>
-        <Button>Home</Button>
-        <Button>About</Button>
-        <Button>Docs</Button>
-      </HStack>
+      <Flex p={2}>
+        <HStack>
+          <Logo />
+          <Button size="sm">Templates</Button>
+          <Button size="sm">Docs</Button>
+          <Button size="sm">Pricing</Button>
+        </HStack>
+        <Spacer />
+        <Button size="sm">Login</Button>
+      </Flex>
       <Outlet />
     </>
   );
