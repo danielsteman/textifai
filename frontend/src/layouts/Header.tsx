1c3888fabc44ba325af0f9deb55adee3a2502e1d
danielsteman <daniel-steman@live.nl>
2023-04-01 10:49:58 +0700

    refactor promo tiara

--- frontend/src/layouts/Header.tsx
+++ frontend/src/layouts/Header.tsx
@@ -1,9 +1,12 @@
+import { SmallCloseIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
   ButtonGroup,
   Center,
   Flex,
+  HStack,
+  Icon,
   Spacer,
   Text,
 } from "@chakra-ui/react";
@@ -13,6 +16,7 @@ import AccountMenu from "../components/AccountMenu";
 import LoginOrRegisterModal from "../components/LoginOrRegisterModal";
 import Logo from "../components/Logo";
 import NavigationButton from "../components/NavigationButton";
+import PromoTiara from "../components/PromoTiara";
 import { AuthContext } from "../providers/AuthProvider";
 
 const Header = () => {
@@ -22,22 +26,7 @@ const Header = () => {
   return (
     <>
       {showPromo && (
-        <Box w="100%" p={2} bgGradient="linear(to-l, #7928CA, #FF0080)">
-          <Center gap={4}>
-            <Text size="sm" color={"white"}>
-              Check out our promo 💎
-            </Text>
-            <Button
-              size="sm"
-              variant={"solid"}
-              bgColor="black"
-              textColor={"white"}
-              rounded={2}
-            >
-              Learn more
-            </Button>
-          </Center>
-        </Box>
+        <PromoTiara />
       )}
       <Flex py={2} px={8} gap={4} direction="row" alignItems="center">
         <Logo />
