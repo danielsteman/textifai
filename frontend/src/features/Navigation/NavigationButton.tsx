2d6547b50826c71fbee50c4870d5a1eef1c88c76
danielsteman <daniel-steman@live.nl>
2023-05-11 08:48:03 +0200

    :construction: upgrade navigation buttons

--- frontend/src/features/Navigation/NavigationButton.tsx
+++ frontend/src/features/Navigation/NavigationButton.tsx
@@ -1,5 +1,5 @@
-import { ArrowDownIcon } from "@chakra-ui/icons";
-import { Button } from "@chakra-ui/react";
+import { ArrowDownIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { NavLink, To } from "react-router-dom";
 
 // TODO: allow NavigationButton to become Menu with MenuItems that navigate() to a page according to its children
@@ -19,9 +19,16 @@ const NavigationButton: React.FC<Props> = ({
 }) => (
   <>
     {subitems ? (
-      <Button size="sm" as={NavLink} to={to} rightIcon={<ArrowDownIcon />}>
-        {title}
-      </Button>
+      <Menu>
+        <MenuButton size="sm" as={Button} rightIcon={<ChevronDownIcon />}>
+          {title}
+        </MenuButton>
+        <MenuList>
+          {subitems.map((item, index) => (
+            <MenuItem key={index}>{item}</MenuItem>
+          ))}
+        </MenuList>
+      </Menu>
     ) : (
       <Button size="sm" as={NavLink} to={to}>
         {title}
