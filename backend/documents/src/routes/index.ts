aa34afc0639960544d8ea94d9047146f7855201b
Daniel Steman <daniel-steman@live.nl>
2023-05-14 19:24:51 +0200

    :art: fix

--- backend/documents/src/routes/index.ts
+++ backend/documents/src/routes/index.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server asfasf ");
+  res.send("Express + TypeScript Server");
 });
 
 export default router;
