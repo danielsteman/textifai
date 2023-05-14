87c8dcb0bdd15f3799c46fe8ee0d0949780dc227
Daniel Steman <daniel-steman@live.nl>
2023-05-14 17:54:25 +0200

    :memo: added swagger doc

--- backend/documents/src/index.ts
+++ backend/documents/src/index.ts
@@ -1,9 +1,11 @@
-import express, { Express, NextFunction, Request, Response } from "express";
+import express, { Express } from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import router from "./routes";
+import YAML from "yaml";
+import fs from "fs";
 
 dotenv.config();
 
@@ -12,7 +14,21 @@ const port = process.env.PORT || 3000;
 const app: Express = express();
 app.use(cors());
 app.use(morgan("tiny"));
-app.use(express.static("files"));
+app.use(express.static("public"));
+
+const swaggerFile: any = process.cwd() + "/swagger.yaml";
+const swaggerData: any = fs.readFileSync(swaggerFile, "utf8");
+const swaggerDocument = YAML.parse(swaggerData);
+
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    swaggerOptions: {
+      url: "/swagger.yaml",
+    },
+  })
+);
 
 app.use(router);
 
