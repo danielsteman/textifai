c1edf9d04f882043742796ea7e49a5c01a4a0a55
Daniel Steman <daniel-steman@live.nl>
2023-05-15 16:11:51 +0000

    :construction: added chat service wip

---
+++ backend/chat/src/index.ts
@@ -0,0 +1,38 @@
+import express, { Express } from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import router from "./routes";
+import YAML from "yaml";
+import fs from "fs";
+
+dotenv.config();
+
+const port = process.env.PORT || 3001;
+
+const app: Express = express();
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(express.static("public"));
+
+const swaggerFile: any = process.cwd() + "/swagger.yaml";
+const swaggerData: any = fs.readFileSync(swaggerFile, "utf8");
+const swaggerDocument = YAML.parse(swaggerData);
+
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    swaggerOptions: {
+      url: "/swagger.yaml",
+    },
+  })
+);
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
---
+++ backend/chat/src/routes/index.ts
@@ -0,0 +1,36 @@
+import express, { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { Configuration, OpenAIApi } from "openai";
+
+const router = express.Router();
+
+const envPath = path.resolve(__dirname, "../../../.env.local");
+
+dotenv.config({ path: envPath });
+
+router.post(
+  "/ask",
+  async (
+    req: Request<{ prompt: string }>,
+    res: Response,
+    next: NextFunction
+  ) => {
+    console.log(process.env.OPENAI_API_KEY);
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    try {
+      const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: req.body.prompt,
+      });
+      res.json({ answer: completion.data.choices[0].text });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+export default router;
