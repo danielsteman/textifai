6eddfbce082a317d04720f9fcffc0c209718707a
danielsteman <daniel-steman@live.nl>
2023-05-16 22:41:06 +0200

    :bricks: create service account

--- infrastructure/project.tf
+++ infrastructure/project.tf
@@ -18,11 +18,37 @@ resource "google_service_account" "service_account" {
   display_name = "Terraform"
 }
 
+data "google_client_openid_userinfo" "gcloud-user" {
+  provider = google-beta.gcloud-user
+}
+
 resource "google_service_account_iam_member" "grant-token-iam" {
   provider           = google-beta.gcloud-user
   service_account_id = google_service_account.service_account.id
   role               = "roles/iam.serviceAccountTokenCreator"
   member             = "user:${data.google_client_openid_userinfo.gcloud-user.email}"
+}
+
+resource "time_sleep" "delay_token_creation" {
+  depends_on = [
+    google_service_account_iam_member.grant-token-iam,
+    google_service_account.service_account,
+  ]
+
+  create_duration = "30s"
+}
+
+data "google_service_account_access_token" "default" {
+  provider = google-beta.gcloud-user
+  #   project                = google_project.default.project_id
+  target_service_account = google_service_account.service_account.email
+  scopes                 = ["userinfo-email", "cloud-platform"]
+  lifetime               = "300s"
+  depends_on = [
+    google_service_account_iam_member.grant-token-iam,
+    google_service_account.service_account,
+    time_sleep.delay_token_creation
+  ]
 }
 
 resource "google_project_service" "default" {
@@ -32,13 +58,14 @@ resource "google_project_service" "default" {
     "cloudbilling.googleapis.com",
     "cloudresourcemanager.googleapis.com",
     "firebase.googleapis.com",
+    "firebasestorage.googleapis.com",
     "serviceusage.googleapis.com",
+    "identitytoolkit.googleapis.com",
   ])
   service = each.key
 
   disable_on_destroy = false
 }
-
 
 resource "google_firebase_project" "default" {
   provider = google-beta.gcloud-user
