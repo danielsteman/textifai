dd80872caa2e349bcc8586e25bfe44066a1a6ee1
danielsteman <daniel-steman@live.nl>
2023-05-27 23:14:43 +0200

    :sparkles: use react drop zone

--- frontend/src/features/UploadForm/UploadForm.tsx
+++ frontend/src/features/UploadForm/UploadForm.tsx
@@ -1,72 +1,21 @@
-import { Box, Button, Center, Input, Text } from "@chakra-ui/react";
-import axios from "axios";
-import { useState } from "react";
-import { MdOutlineCloudUpload } from "react-icons/md";
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 
 const UploadForm = () => {
-  const [file, setFile] = useState();
-  const [fileName, setFileName] = useState("");
-
-  const [dragActive, setDragActive] = useState(false);
-
-  const handleDrag = function (e: any) {
-    e.preventDefault();
-    e.stopPropagation();
-    if (e.type === "dragenter" || e.type === "dragover") {
-      setDragActive(true);
-    } else if (e.type === "dragleave") {
-      setDragActive(false);
-    }
-  };
-
-  const saveFile = (e: any) => {
-    setFile(e.target.files[0]);
-    setFileName(e.target.files[0].name);
-  };
-
-  const uploadFile = async (e: any) => {
-    const formData = new FormData();
-    if (file && fileName) {
-      formData.append("file", file);
-      formData.append("fileName", fileName);
-    } else {
-      throw new Error("No file was found");
-    }
-    try {
-      const res = await axios.post(
-        "http://localhost:3000/api/documents/upload",
-        formData
-      );
-      console.log(res);
-    } catch (ex) {
-      console.log(ex);
-    }
-  };
+  const onDrop = useCallback((acceptedFiles: any) => {
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <Center>
-      <form onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
-        <Box
-          borderColor="grey"
-          borderWidth={1}
-          w="fit-content"
-          p={8}
-          flex={1}
-          bgColor={dragActive ? "green.100" : "green.200"}
-        >
-          <Input
-            type="file"
-            onChange={saveFile}
-            multiple={true}
-            display="none"
-          />
-          <MdOutlineCloudUpload size={36} />
-          <Button onClick={uploadFile}>Click to upload</Button>
-          <Text>or drag and drop</Text>
-          <Text>PDF up to 2MB</Text>
-        </Box>
-      </form>
-    </Center>
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
+    </div>
   );
 };
 
