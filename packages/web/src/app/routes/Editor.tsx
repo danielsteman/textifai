0a7cc486b4b3d3f6b86e620398529f44218d5beb
danielsteman <daniel-steman@live.nl>
2023-05-29 16:02:51 +0200

    :sparkles: add editor

--- packages/web/src/app/routes/Editor.tsx
+++ packages/web/src/app/routes/Editor.tsx
@@ -1,11 +1,37 @@
-import React, { useState } from "react";
+import { Box, Grid, GridItem } from "@chakra-ui/react";
+import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import Chat from "../../features/Chat/Chat";
 
 function Editor() {
   const [value, setValue] = useState("");
 
-  return <ReactQuill theme="snow" value={value} onChange={setValue} />;
+  return (
+    <>
+      <Grid
+        templateColumns="repeat(3, 1fr)"
+        templateRows="repeat(2, 1fr)"
+        gap={4}
+        flex={1}
+      >
+        <GridItem colSpan={2} rowSpan={2} p={4}>
+          <ReactQuill
+            theme="snow"
+            value={value}
+            onChange={setValue}
+            style={{ height: "100%", width: "100%" }}
+          />
+        </GridItem>
+        <GridItem colSpan={1} rowSpan={1}>
+          <Box>Document list</Box>
+        </GridItem>
+        <GridItem colSpan={1} rowSpan={1}>
+          <Chat />
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
 export default Editor;
