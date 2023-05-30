6eaae89fb57d8c54fecb940717857a2a0985343e
Daniel Steman <daniel-steman@live.nl>
2023-05-30 21:54:06 +0200

    :construction: upload wip

---
+++ packages/documents/src/config/firebase.ts
@@ -0,0 +1,3 @@
+const admin = require("firebase-admin");
+
+// TODO: add firebase storage credentials
--- packages/documents/src/routes/index.ts
+++ packages/documents/src/routes/index.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.post("/upload", (req: Request, res: Response) => {
+  console.log(req.body);
+
   res.send("Got a POST request");
 });
 
