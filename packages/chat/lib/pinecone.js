67d9bd07add1981fb4895c8b687f7af187ed1a50
Thijs Daniels <thijs.daniels@hotmail.com>
2023-08-04 14:36:02 +0200

    :closed_lock_with_key: replaced api keys with env variables

--- packages/chat/lib/pinecone.js
+++ packages/chat/lib/pinecone.js
@@ -1,13 +1,20 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const pinecone_1 = require("@pinecone-database/pinecone");
+const dotenv_1 = __importDefault(require("dotenv"));
+const path_1 = __importDefault(require("path"));
+const envPath = path_1.default.resolve(__dirname, "../../.env.local");
+dotenv_1.default.config({ path: envPath });
 const client = new pinecone_1.PineconeClient();
 async function initializeClient() {
     await client.init({
-        apiKey: "73144a3a-1736-4c50-a992-8fb87cdeacb0",
-        environment: "northamerica-northeast1-gcp"
+        apiKey: process.env.PINECONE_API_KEY,
+        environment: process.env.PINECONE_ENV
     });
-    const pineconeIndex = client.Index("textifai");
+    const pineconeIndex = client.Index(process.env.PINECONE_INDEX);
     return pineconeIndex;
 }
 exports.default = initializeClient;
