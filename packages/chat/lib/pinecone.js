5c060a9e36a540bd810343c0d69a1a80522a1196
Daniel Steman <daniel-steman@live.nl>
2023-08-15 14:39:45 +0000

    :bug: fix

--- packages/chat/lib/pinecone.js
+++ packages/chat/lib/pinecone.js
@@ -6,34 +6,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const pinecone_1 = require("@pinecone-database/pinecone");
 const dotenv_1 = __importDefault(require("dotenv"));
 const path_1 = __importDefault(require("path"));
-const envPath = path_1.default.resolve(__dirname, "../../.env.local");
+const envPath = path_1.default.resolve(__dirname, "../.env.local");
 dotenv_1.default.config({ path: envPath });
-const client = new pinecone_1.PineconeClient();
+console.log(process.env.PINECONE_ENV);
+if (!process.env.PINECONE_ENV || !process.env.PINECONE_API_KEY) {
+    throw new Error("Pinecone environment or API key vars missing");
+}
 async function initializeClient() {
-    await client.init({
-        apiKey: process.env.PINECONE_API_KEY,
-        environment: process.env.PINECONE_ENV
-    });
-    const pineconeIndex = client.Index(process.env.PINECONE_INDEX);
-    return pineconeIndex;
+    try {
+        const client = new pinecone_1.PineconeClient();
+        await client.init({
+            apiKey: process.env.PINECONE_API_KEY,
+            environment: process.env.PINECONE_ENV,
+        });
+        const pineconeIndex = client.Index(process.env.PINECONE_INDEX);
+        return pineconeIndex;
+    }
+    catch (error) {
+        console.error("An error occurred while initializing the Pinecone client:", error);
+        throw new Error("Failed to initialize Pinecone Client");
+    }
 }
 exports.default = initializeClient;
-// import { PineconeClient } from "@pinecone-database/pinecone";
-// import dotenv from "dotenv";
-// import path from "path";
-// const envPath = path.resolve(__dirname, "../../.env.local");
-// dotenv.config({ path: envPath })
-// export async function initPinecone() {
-//   try {
-//     const pinecone = new PineconeClient();
-//     await pinecone.init({
-//       apiKey: process.env.PINECONE_API_KEY!,
-//       environment: process.env.PINECONE_ENVIRONMENT!, 
-//     });
-//     console.log("Successfully initiated Pinecone")
-//     return pinecone;
-//   } catch (error) {
-//     console.log("error", error);
-//     throw new Error("Failed to initialize Pinecone Client");
-//   }
-// }
