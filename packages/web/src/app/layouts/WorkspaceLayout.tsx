c9b60f22e1bab955f978561835f90a3f926bbc75
Daniel Steman <daniel-steman@live.nl>
2023-08-23 12:22:52 +0000

    :construction: wip

---
+++ packages/web/src/app/layouts/WorkspaceLayout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WorkspaceLayout = () => {
+  return <div></div>;
+};
+
+export default WorkspaceLayout;
--- packages/web/src/app/routes/Workspace.tsx
+++ packages/web/src/app/routes/Workspace.tsx
@@ -1,21 +1,35 @@
-import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
+import { SettingsIcon } from "@chakra-ui/icons";
+import {
+  ButtonGroup,
+  IconButton,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
 
 const Workspace = () => {
   return (
-    <Tabs isFitted variant="enclosed">
-      <TabList mb="1em">
-        <Tab>One</Tab>
-        <Tab>Two</Tab>
-      </TabList>
-      <TabPanels>
-        <TabPanel>
-          <p>one!</p>
-        </TabPanel>
-        <TabPanel>
-          <p>two!</p>
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
+    <>
+      <ButtonGroup>
+        <IconButton aria-label={"settings"} icon={<SettingsIcon />} />
+      </ButtonGroup>
+      <Tabs isFitted variant="enclosed">
+        <TabList mb="1em">
+          <Tab>One</Tab>
+          <Tab>Two</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <p>one!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </>
   );
 };
 
