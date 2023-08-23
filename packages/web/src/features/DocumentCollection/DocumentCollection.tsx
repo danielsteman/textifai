2fe780318988a94f2bac162099e1d23e31938a61
Daniel Steman <daniel-steman@live.nl>
2023-08-23 11:50:48 +0000

    :construction: add input field

--- packages/web/src/features/DocumentCollection/DocumentCollection.tsx
+++ packages/web/src/features/DocumentCollection/DocumentCollection.tsx
@@ -1,8 +1,15 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../app/providers/AuthProvider";
-import { Text, VStack } from "@chakra-ui/react";
+import {
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { storage } from "../../app/config/firebase";
 import { StorageReference, listAll, ref } from "firebase/storage";
+import { SearchIcon } from "@chakra-ui/icons";
 
 const DocumentCollection = () => {
   const currentUser = useContext(AuthContext);
@@ -22,6 +29,12 @@ const DocumentCollection = () => {
 
   return (
     <VStack bgColor={"lightgrey"}>
+      <InputGroup>
+        <InputLeftElement pointerEvents="none">
+          <SearchIcon />
+        </InputLeftElement>
+        <Input placeholder="Search" />
+      </InputGroup>
       {documents.map((doc) => (
         <Text key={doc.fullPath}>{doc.name}</Text>
       ))}
