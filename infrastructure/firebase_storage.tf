daaf065bf623f95635f24e25a3d74194d6c1c214
Daniel Steman <daniel-steman@live.nl>
2023-09-01 16:24:48 +0200

    :bricks: Create firestore

--- infrastructure/firebase_storage.tf
+++ infrastructure/firebase_storage.tf
@@ -1,14 +1,8 @@
-resource "google_app_engine_application" "app" {
-  project     = google_project.default.project_id
-  location_id = "europe-west"
-  database_type = "CLOUD_FIRESTORE"
-}
-
 resource "google_firestore_database" "database" {
-  project     = google_project.project.project_id
-  name        = "(default)"
-  location_id = "nam5"
+  project     = google_project.default.project_id
+  name        = var.project_name
+  location_id = "eur3"
   type        = "FIRESTORE_NATIVE"
 
-  depends_on = [google_project_service.firestore]
+  depends_on = [google_project_service.default]
 }
