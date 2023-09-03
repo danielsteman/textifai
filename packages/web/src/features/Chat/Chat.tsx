b6bdb15a94fb0f948e76527ba6c9ddeda5107544
danielsteman <daniel-steman@live.nl>
2023-09-03 22:30:12 +0200

    :bug: fix chat component height

--- packages/web/src/features/Chat/Chat.tsx
+++ packages/web/src/features/Chat/Chat.tsx
@@ -14,9 +14,8 @@ import {
   SkeletonCircle,
   Spacer,
   Text,
-  useDisclosure,
 } from "@chakra-ui/react";
-import { RepeatIcon, PlusSquareIcon } from '@chakra-ui/icons';
+import { RepeatIcon, PlusSquareIcon } from "@chakra-ui/icons";
 import React, { useEffect, useRef, useState } from "react";
 import { MdSend } from "react-icons/md";
 import { v4 as uuidv4 } from "uuid";
@@ -67,7 +66,7 @@ const Chat = () => {
       const res = await axios.post("http://localhost:3001/api/chat/ask", {
         prompt: message,
         history: history,
-        option: "GeneralQa"
+        option: "GeneralQa",
       });
       setAnswerStack([...answerStack, res.data.answer]);
       setLoading(false);
@@ -76,18 +75,15 @@ const Chat = () => {
     }
   };
 
-  const handleRegenerate = async (
-      option: string,
-      originalMessage: string
-    ) => {
+  const handleRegenerate = async (option: string, originalMessage: string) => {
     // Capture the last message from answerStack
     const lastSystemMessage = answerStack[answerStack.length - 1];
 
     try {
       // Make the API call with the last system message
       const res = await axios.post("http://localhost:3001/api/chat/ask", {
-        prompt: lastSystemMessage,  // Sending the last system message
-        option: "regenerate",  // The option is set to "regenerate"
+        prompt: lastSystemMessage, // Sending the last system message
+        option: "regenerate", // The option is set to "regenerate"
       });
 
       // Replace the last message in answerStack with the regenerated one
@@ -96,14 +92,14 @@ const Chat = () => {
         updatedAnswers[updatedAnswers.length - 1] = res.data.answer;
         return updatedAnswers;
       });
-      } catch (error) {
-        console.log(error);
-      }
-    };
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const SystemMessage = ({ message }: SystemMessageProps) => {
-    const [menuClicked, setMenuClicked] = useState(false);  // Add state to track menu click
-  
+    const [menuClicked, setMenuClicked] = useState(false); // Add state to track menu click
+
     return (
       <Flex align="left" justifyContent="flex-start" flexDirection="column">
         <Box
@@ -115,24 +111,30 @@ const Chat = () => {
           rounded={8}
           position="relative"
         >
-          <Text whiteSpace="pre-line">
-            {message}
-          </Text>
-          {!menuClicked && (  // Conditionally render menu based on state
+          <Text whiteSpace="pre-line">{message}</Text>
+          {!menuClicked && ( // Conditionally render menu based on state
             <Box position="absolute" right={2} top={0}>
               <Menu>
                 <MenuButton
                   as={IconButton}
                   aria-label="Options"
-                  icon={<PlusSquareIcon/>}  // Use the new icon
+                  icon={<PlusSquareIcon />} // Use the new icon
                   variant="ghost"
                   size="sm"
                 />
                 <MenuList>
-                  <MenuItem onClick={() => { setMenuClicked(true); /* Your logic here */ }}>
+                  <MenuItem
+                    onClick={() => {
+                      setMenuClicked(true); /* Your logic here */
+                    }}
+                  >
                     Copy to Working Document
                   </MenuItem>
-                  <MenuItem onClick={() => { setMenuClicked(true); /* Your logic here */ }}>
+                  <MenuItem
+                    onClick={() => {
+                      setMenuClicked(true); /* Your logic here */
+                    }}
+                  >
                     Show in Source Document
                   </MenuItem>
                 </MenuList>
@@ -143,50 +145,83 @@ const Chat = () => {
       </Flex>
     );
   };
-  
+
   return (
-    <Flex flexDir="column" flex={1} p={8} h="100vh" overflowY="hidden">
-    <Box mb={4} flex="1" overflowY="scroll" overflowX="hidden">
-      {messageStack.map((msg, index) => (
-        <Box key={uuidv4()} py={2}>
-          <Flex mb={2}>
-            <Spacer />
-            <Text bgColor="teal" p={1} px={4} rounded={4}>
-              {msg}
-            </Text>
-          </Flex>
-          {loading && index === messageStack.length - 1 ? (
-            <HStack>
-              <SkeletonCircle size="2" />
-              <SkeletonCircle size="2" />
-              <SkeletonCircle size="2" />
-            </HStack>
-          ) : (
-            <>
-              <SystemMessage message={answerStack[index]} />
-              {index === answerStack.length - 1 && messageStack.length === answerStack.length && (
-                <Flex justifyContent="center" alignItems="center" py={4}>
-                  <Button leftIcon={< RepeatIcon />} 
-                      onClick={() => handleRegenerate("regenerate", message)}>
-                      Regenerate
-                  </Button>
-                </Flex>
-              )}
-            </>
-          )}
-        </Box>
-      ))}
-      <Box ref={messagesEndRef} />
-    </Box>
+    <Flex flexDir="column" flex={1} p={8} overflowY="hidden" h="100%">
+      <Box mb={4} overflowY="scroll" overflowX="hidden" h="100%">
+        {messageStack.map((msg, index) => (
+          <Box key={uuidv4()} py={2}>
+            <Flex mb={2}>
+              <Spacer />
+              <Text bgColor="teal" p={1} px={4} rounded={4}>
+                {msg}
+              </Text>
+            </Flex>
+            {loading && index === messageStack.length - 1 ? (
+              <HStack>
+                <SkeletonCircle size="2" />
+                <SkeletonCircle size="2" />
+                <SkeletonCircle size="2" />
+              </HStack>
+            ) : (
+              <>
+                <SystemMessage message={answerStack[index]} />
+                {index === answerStack.length - 1 &&
+                  messageStack.length === answerStack.length && (
+                    <Flex justifyContent="center" alignItems="center" py={4}>
+                      <Button
+                        leftIcon={<RepeatIcon />}
+                        onClick={() => handleRegenerate("regenerate", message)}
+                      >
+                        Regenerate
+                      </Button>
+                    </Flex>
+                  )}
+              </>
+            )}
+          </Box>
+        ))}
+        <Box ref={messagesEndRef} />
+      </Box>
       {messageStack.length === 0 && (
-        <Flex flexDirection="column" alignItems="center" justifyContent="center" mb={4} pb={6}>
+        <Flex
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          mb={4}
+          pb={6}
+        >
           <HStack mb={2}>
-            <Button onClick={() => {/* Your logic here */}}>Sample Question 1</Button>
-            <Button onClick={() => {/* Your logic here */}}>Sample Question 2</Button>
+            <Button
+              onClick={() => {
+                /* Your logic here */
+              }}
+            >
+              Sample Question 1
+            </Button>
+            <Button
+              onClick={() => {
+                /* Your logic here */
+              }}
+            >
+              Sample Question 2
+            </Button>
           </HStack>
           <HStack>
-            <Button onClick={() => {/* Your logic here */}}>Sample Question 3</Button>
-            <Button onClick={() => {/* Your logic here */}}>Sample Question 4</Button>
+            <Button
+              onClick={() => {
+                /* Your logic here */
+              }}
+            >
+              Sample Question 3
+            </Button>
+            <Button
+              onClick={() => {
+                /* Your logic here */
+              }}
+            >
+              Sample Question 4
+            </Button>
           </HStack>
         </Flex>
       )}
@@ -210,7 +245,7 @@ const Chat = () => {
         </InputGroup>
       </form>
     </Flex>
-  );  
+  );
 };
 
 export default Chat;
