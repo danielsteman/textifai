508adb272259832704f4629a6c35457dc1201570
danielsteman <daniel-steman@live.nl>
2023-09-04 11:17:40 +0200

    :sparkles: make bgcolor of panelwrapper dynamic

--- packages/web/src/features/WorkspaceTabs/PanelWrapper.tsx
+++ packages/web/src/features/WorkspaceTabs/PanelWrapper.tsx
@@ -1,15 +1,23 @@
-import { Grid, GridItem, TabPanel } from "@chakra-ui/react";
+import { Grid, GridItem, TabPanel, useColorMode } from "@chakra-ui/react";
 import { ITab } from "../../app/routes/Workspace";
 import MiniLibraryPanel from "./MiniLibraryPanel";
 import ChatPanel from "./ChatPanel";
+import theme from "../../app/themes/theme";
 
 interface Props {
   tab: ITab;
 }
 
 const PanelWrapper: React.FC<Props> = (props) => {
+  const { colorMode } = useColorMode();
   return (
-    <TabPanel h="100%" flex="1" bgColor={"black"} borderRadius={16} p={2}>
+    <TabPanel
+      h="100%"
+      flex="1"
+      bgColor={theme.colors[colorMode].surfaceContainer}
+      borderRadius={16}
+      p={2}
+    >
       <Grid
         h="100%"
         templateRows="repeat(2, 1fr)"
