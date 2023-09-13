276d8a156e2b6e83ccabaa6163c3834ebc7b0a61
Daniel Steman <daniel-steman@live.nl>
2023-09-13 08:05:46 +0000

    :construction: wip

--- packages/web/src/features/PdfViewer/SinglePdfViewer.tsx
+++ packages/web/src/features/PdfViewer/SinglePdfViewer.tsx
@@ -1,28 +1,43 @@
 import { Box } from "@chakra-ui/react";
-import { useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Document, Page } from "react-pdf";
-import { getDownloadURL, ref, StorageReference } from "firebase/storage";
+import { getDownloadURL, StorageReference } from "firebase/storage";
 
 interface Props {
   document: StorageReference;
 }
 
-const SinglePdfViewer: React.FC<Props> = async ({ document }) => {
+const SinglePdfViewer: React.FC<Props> = ({ document }) => {
   const [numPages, setNumPages] = useState<number>(0);
-  const pdfRef = useRef<HTMLDivElement>(null);
+  const [scale, setScale] = useState<number>(1);
+  const [pdfURL, setPdfURL] = useState<string | null>(null);
 
-  const pdfURL = await getDownloadURL(document);
+  useEffect(() => {
+    const fetchDocumentUrl = async () => {
+      try {
+        const pdfURL = await getDownloadURL(document);
+        setPdfURL(pdfURL);
+      } catch (error) {
+        console.error("Error fetching PDF URL", error);
+      }
+    };
+
+    if (document) {
+      fetchDocumentUrl();
+    }
+  }, [document]);
+
   return (
-    <Box flex="1" padding="1rem" ref={pdfRef} overflowY="auto">
+    <Box flex="1" padding="1rem" overflowY="auto">
       <Box
         display="flex"
         flexDirection="column"
         alignItems="center"
         minHeight="100%"
       >
-        {document && (
+        {pdfURL && (
           <Document
-            file={document}
+            file={pdfURL}
             onLoadSuccess={({ numPages }) => setNumPages(numPages)}
           >
             {Array.from({ length: numPages }, (_, index) => (
