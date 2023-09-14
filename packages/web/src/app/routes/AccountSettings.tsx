171bb7a6286ce66841eb676c3757b09fbdafa8ac
Thijs Daniels <thijs.daniels@hotmail.com>
2023-09-14 15:17:19 +0200

    :construction: WIP add upload profile picture functionality

--- packages/web/src/app/routes/AccountSettings.tsx
+++ packages/web/src/app/routes/AccountSettings.tsx
@@ -6,79 +6,129 @@ import {
     Input,
     Button,
     Avatar,
+    Icon,
+    IconButton,
+    VStack,
     HStack
   } from "@chakra-ui/react";
-  import SidebarMenu from "../../common/components/SidebarMenu";
-  
+  import { AiOutlineArrowUp, AiFillExclamationCircle } from "react-icons/ai";
+  import React, { useRef } from 'react';
+
   const AccountSettings = () => {
     const user = {
       avatarUrl: "URL_OF_USER_AVATAR",
-      firstName: "Henk",
-      lastName: "de Tank",
-      email: "henk.detank@example.com",
+      firstName: "John",
+      lastName: "Doe",
+      email: "john.doe@example.com",
       emailVerified: false
+    };
+
+    const fileInputRef = React.useRef<HTMLInputElement>(null);
+
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files ? e.target.files[0] : null;
+        if (file) {
+            console.log(file);
+        }
     };
   
     return (
-        <Box p={6} boxShadow="base" borderRadius="md" bg="white" flex="1">
-          <Text fontSize="2xl" mb={4}>Account settings</Text>
+      <Box p={6} boxShadow="base" borderRadius="md" bg="white" w="50%" m="auto">
+        <Text fontSize="2xl" mb={4}>Account settings</Text>
   
-          {/* Profile Section */}
-          <Text fontSize="xl" mb={4}>Profile</Text>
-          <Flex alignItems="center" mb={4}>
-            <Avatar name={`${user.firstName} ${user.lastName}`} src={user.avatarUrl} />
-            <Button ml={4}>Change Avatar</Button>
+        {/* Profile Section */}
+        <Text fontSize="xl" mb={4}>Profile</Text>
+        
+        <VStack spacing={4} alignItems="start" mb={4}>
+          <Box position="relative">
+            <Avatar size="2xl" name={`${user.firstName} ${user.lastName}`} src={user.avatarUrl} />
+            <input
+                ref={fileInputRef}
+                type="file"
+                accept="image/*"
+                onChange={handleFileChange}
+                style={{ display: 'none' }} // hide the input field
+            />
+            <IconButton 
+                icon={<Icon as={AiOutlineArrowUp}/>} 
+                position="absolute" 
+                bottom={1} 
+                right={1} 
+                backgroundColor="white" 
+                borderColor="black"
+                borderWidth="1px"
+                isRound 
+                aria-label="Upload Image" 
+                onClick={() => fileInputRef.current?.click()}
+            />
+          </Box>
+          
+          <Flex direction="column">
+            <Text mb={2}>First name</Text>
+            <Input variant="flushed" borderRadius="sm" placeholder={user.firstName} w="60%" />
           </Flex>
-          <Box mb={4}>
-            <Text mb={2}>First Name</Text>
-            <Input size="sm" placeholder="First Name" borderRadius="md" defaultValue={user.firstName} />
-          </Box>
-          <Box mb={4}>
-            <Text mb={2}>Last Name</Text>
-            <Input size="sm" placeholder="Last Name" borderRadius="md" defaultValue={user.lastName} />
-          </Box>
+          
+          <Flex direction="column">
+            <Text mb={2}>Last name</Text>
+            <Input variant="flushed" borderRadius="sm" placeholder={user.lastName} w="60%" />
+          </Flex>
+        </VStack>
   
-          <Divider my={6} />
+        <Divider mb={4} />
   
-          {/* Email and PhoneNumber Section */}
-          <Text fontSize="xl" mb={4}>Email & Phone</Text>
-          <Box mb={4}>
-            <Text mb={2}>Email</Text>
-            <Input size="sm" type="email" placeholder="Email" borderRadius="md" defaultValue={user.email} />
-            {!user.emailVerified && <Text color="red.500">Email not verified</Text>}
-          </Box>
-          <Box mb={4}>
-            <Text mb={2}>Phone Number</Text>
-            <Input size="sm" type="tel" placeholder="Phone Number" />
-          </Box>
+        {/* Email Section */}
+        <Text fontSize="xl" mb={4}>Email and Phone</Text>
+        <VStack spacing={4} mb={4} align="start">
+        <Text mb={2}>Email</Text>
+        <Input variant="flushed" borderRadius="sm" placeholder={user.email} w="60%" />
+        {
+          user.emailVerified ? (
+            <Button colorScheme="blue" mt={2}>Update email</Button>
+          ) : (
+            <Button colorScheme="orange" mt={2} leftIcon={<Icon as={AiFillExclamationCircle} />}>Send verification mail</Button>
+          )
+        }
+
+        <Text mb={2}>Phone Number</Text>
+        <Input variant="flushed" borderRadius="sm" placeholder="Enter phone number" w="60%" />
+        <Button colorScheme="blue" mt={2}>Update phone number</Button>
+      </VStack>
   
-          <Divider my={6} />
+        <Divider mb={4} />
   
-          {/* Change Password Section */}
-          <Text fontSize="xl" mb={4}>Change Password</Text>
-          <Box mb={4}>
-            <Text mb={2}>Old Password</Text>
-            <Input size="sm" type="password" borderRadius="md" placeholder="Old Password" />
-          </Box>
-          <Box mb={4}>
-            <Text mb={2}>New Password</Text>
-            <Input size="sm" type="password" borderRadius="md" placeholder="New Password" />
-          </Box>
-          <Box mb={4}>
-            <Text mb={2}>Confirm Password</Text>
-            <Input size="sm" type="password" borderRadius="md" placeholder="Confirm Password" />
-          </Box>
+        <Text fontSize="xl" mb={4}>Password</Text>
+        {/* Password Section */}
+        <Flex direction="column" mb={4}>
+          <Text mb={2}>Current password</Text>
+          <Input variant="flushed" type="password" borderRadius="sm" placeholder="******" w="60%" />
+        </Flex>
   
-          <Divider my={6} />
+        {/* New Password Section */}
+        <Flex direction="column" mb={4}>
+          <Text mb={2}>New password</Text>
+          <Input variant="flushed" type="password" borderRadius="sm" placeholder="******" w="60%" />
+        </Flex>
   
-          {/* Delete Account */}
-          <Text color="red.500" fontSize="xl" mb={4}>
+        {/* Password Confirmation Section */}
+        <Flex direction="column" mb={4}>
+          <Text mb={2}>Confirm new password</Text>
+          <Input variant="flushed" type="password" borderRadius="sm" placeholder="******" w="60%" />
+        </Flex>
+  
+        <Button colorScheme="blue" mb={4}>Change password</Button>
+
+        <Divider mb={4} />
+
+        {/* Delete Account Section */}
+        <Text fontSize="xl" mb={4}>Delete Account</Text>
+        <Text fontSize="sm" mb={4}>If you no longer want to use Textifai, you can permenantly delete your account. You can't undo this action.</Text>
+        {/* Delete Account Section */}
+        <Button colorScheme="red" variant="outline" leftIcon={<Icon as={AiFillExclamationCircle} />}>
             Delete Account
-          </Text>
-          <Button colorScheme="red">Delete My Account</Button>
+        </Button>
         </Box>
-    );
-  };
+        );
+    };
   
   export default AccountSettings;
   
