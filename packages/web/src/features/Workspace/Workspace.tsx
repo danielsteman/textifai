48ea30f08ae5440a19eac005e65d0d8eb4c98d57
Daniel Steman <daniel-steman@live.nl>
2023-09-19 16:07:48 +0000

    remove props

--- packages/web/src/features/Workspace/Workspace.tsx
+++ packages/web/src/features/Workspace/Workspace.tsx
@@ -149,7 +149,7 @@ const Workspace = () => {
             onClick={() => {
               const tab: ITab = {
                 name: "Chat",
-                panel: <ChatPanel selectedDocuments={selectedDocuments} />,
+                panel: <ChatPanel />,
                 openChatSupport: false,
                 openMiniLibrary: false,
                 openPdfViewer: false,
