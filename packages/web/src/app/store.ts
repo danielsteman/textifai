0c3f74881d346098167ba03b84ea760b56e6d681
Daniel Steman <daniel-steman@live.nl>
2023-09-19 15:54:17 +0000

    wip

--- packages/web/src/app/store.ts
+++ packages/web/src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import libraryReducer from "../features/DocumentCollection/librarySlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    library: libraryReducer,
+  },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
