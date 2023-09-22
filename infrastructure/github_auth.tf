a6b4d958568ec076036b647429d2b03b9a97eb49
Daniel Steman <daniel-steman@live.nl>
2023-09-22 14:00:23 +0200

    :green_heart: Add service account

---
+++ infrastructure/github_auth.tf
@@ -0,0 +1,28 @@
+resource "google_service_account" "github" {
+  account_id   = "githubactions"
+  display_name = "Github Actions Service Account"
+  project = google_project.default.project_id
+}
+
+resource "google_project_iam_binding" "project" {
+  project = google_project.default.project_id
+  role    = "roles/artifactregistry.reader"
+
+  members = [
+    google_service_account.github.member
+  ]
+}
+
+resource "google_project_iam_binding" "project" {
+  project = google_project.default.project_id
+  role    = "roles/artifactregistry.writer"
+
+  members = [
+    google_service_account.github.member
+  ]
+}
+
+resource "google_iam_workload_identity_pool" "github" {
+  workload_identity_pool_id = "github-pool"
+  project = google_project.default.project_id
+}
