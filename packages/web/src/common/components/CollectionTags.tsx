a51b12368be0aecb640310fbabf1cd220578e4c7
Thijs Daniels <thijs.daniels@hotmail.com>
2023-09-24 14:00:17 +0200

    :sparkles: give tags color coding

--- packages/web/src/common/components/CollectionTags.tsx
+++ packages/web/src/common/components/CollectionTags.tsx
@@ -1,10 +1,15 @@
 import { FC, KeyboardEvent } from 'react';
-import { Box, Tag, TagCloseButton, TagLabel, Input } from "@chakra-ui/react";
+import { Box, Tag, TagCloseButton, TagLabel, Input, VStack, Text } from "@chakra-ui/react";
 
 type TagInputProps = {
   tags?: string[];
   onAddTag: (tag: string) => void;
   onDeleteTag: (tag: string) => void;
+};
+
+const getColorForTag = (tag: string) => {
+  const colors = ["red.500", "blue.500", "green.500", "yellow.500", "purple.500", "pink.500"]; 
+  return colors[tag.charCodeAt(0) % colors.length];
 };
 
 const TagInput: React.FC<TagInputProps> = ({ tags = [], onAddTag, onDeleteTag }) => {
@@ -17,28 +22,35 @@ const TagInput: React.FC<TagInputProps> = ({ tags = [], onAddTag, onDeleteTag })
 
   return (
     <Box>
-      {tags.map((tag, index) => (
-        <Tag
+      <VStack spacing={2} align="start">
+        {tags.map((tag, index) => (
+          <Tag
+            size="sm"
+            key={index}
+            borderRadius="full"
+            variant="solid"
+            bgColor={getColorForTag(tag)}
+          >
+            <TagLabel>{tag}</TagLabel>
+            <TagCloseButton onClick={() => onDeleteTag(tag)} />
+          </Tag>
+        ))}
+        <Input
+          placeholder={tags.length === 0 ? "Add a tag..." : ""}
           size="sm"
-          key={index}
-          borderRadius="full"
-          variant="solid"
-          m={1}
-          bgColor="blue.500"
-        >
-          <TagLabel>{tag}</TagLabel>
-          <TagCloseButton onClick={() => onDeleteTag(tag)} />
-        </Tag>
-      ))}
-      <Input
-        placeholder="Add a tag..."
-        size="sm"
-        onKeyDown={handleKeyDown}
-        variant="flushed"
-        width="auto"
-      />
+          onKeyDown={handleKeyDown}
+          variant= {tags.length === 0 ? "flushed" : "unstyled"}
+          width="auto"
+        />
+      </VStack>
     </Box>
   );
 };
 
 export default TagInput;
+
+
+
+
+
+
