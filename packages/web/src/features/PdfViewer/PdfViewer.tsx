64cde95a77c2d5838e768d5eab68ad924a992d1d
Thijs Daniels <thijs.daniels@hotmail.com>
2023-09-26 21:21:00 +0200

    :sparkles: add zoom function for pdf-viewer

--- packages/web/src/features/PdfViewer/PdfViewer.tsx
+++ packages/web/src/features/PdfViewer/PdfViewer.tsx
@@ -10,6 +10,7 @@ interface Props {
 const PdfViewer: React.FC<Props> = ({ document }) => {
   const [numPages, setNumPages] = useState<number>(0);
   const [pdfURL, setPdfURL] = useState<string | null>(null);
+  const [scale, setScale] = useState<number>(1);
 
   useEffect(() => {
     const fetchDocumentUrl = async () => {
@@ -26,32 +27,57 @@ const PdfViewer: React.FC<Props> = ({ document }) => {
     }
   }, [document]);
 
+  const zoomIn = () => {
+    setScale((prevScale) => Math.min(prevScale + 0.1, 3));
+  };
+
+  const zoomOut = () => {
+    setScale((prevScale) => Math.max(prevScale - 0.1, 0.5));
+  };
+
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      height="100%"
-      overflow="auto"
-    >
-      {pdfURL && (
-        <Document
-          file={pdfURL}
-          onLoadSuccess={({ numPages }) => setNumPages(numPages)}
-        >
-          {Array.from({ length: numPages }, (_, index) => (
-            <Box
-              key={`page_${index + 1}`}
-              borderBottom="1px solid black"
-              marginBottom="1rem"
-            >
-              <Page pageNumber={index + 1} scale={1} />
-            </Box>
-          ))}
-        </Document>
-      )}
+    <Box position="relative" height="100%" width="100%">
+      {/* Zoom controls */}
+      <Box
+        position="sticky"
+        top="0"
+        p="0.5rem"
+        display="flex"
+        justifyContent="center"
+        boxShadow="sm"
+      >
+        <button onClick={zoomOut}>-</button>
+        <span style={{ margin: '0 1rem' }}>{Math.round(scale * 100)}%</span>
+        <button onClick={zoomIn}>+</button>
+      </Box>
+  
+      {/* PDF content */}
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        height="100%"
+        overflow="auto"
+      >
+        {pdfURL && (
+          <Document
+            file={pdfURL}
+            onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+          >
+            {Array.from({ length: numPages }, (_, index) => (
+              <Box
+                key={`page_${index + 1}`}
+                borderBottom="1px solid black"
+                marginBottom="1rem"
+              >
+                <Page pageNumber={index + 1} scale={scale} />
+              </Box>
+            ))}
+          </Document>
+        )}
+      </Box>
     </Box>
-  );
+  );  
 };
 
 export default PdfViewer;
