6095d20c24c19e7f60b2b93bef337395ed065644
Daniel Steman <daniel-steman@live.nl>
2023-09-29 12:54:13 +0200

    :pencil2: Fixes

--- packages/web/src/features/Workspace/panels/EditorPanel.tsx
+++ packages/web/src/features/Workspace/panels/EditorPanel.tsx
@@ -1,6 +1,18 @@
-import { Button, HStack, Spacer, Text, useColorMode } from "@chakra-ui/react";
+import {
+  Button,
+  CloseButton,
+  HStack,
+  IconButton,
+  Input,
+  Spacer,
+  Text,
+  Tooltip,
+  useColorMode,
+} from "@chakra-ui/react";
 import TextEditor from "../../TextEditor/TextEditor";
 import theme from "../../../app/themes/theme";
+import { useContext, useState } from "react";
+import { ProjectContext } from "../../../app/providers/ProjectProvider";
 
 export interface CustomTabPanelProps {
   openChatSupport: boolean;
@@ -9,6 +21,22 @@ export interface CustomTabPanelProps {
 
 const EditorPanel = () => {
   const { colorMode } = useColorMode();
+  const userProjects = useContext(ProjectContext);
+  const [inputMode, setInputMode] = useState<boolean>(false);
+
+  const getCurrentProjectTitle = () => {
+    const projectTitle = userProjects.filter(
+      (project) => project.active === true
+    );
+    if (projectTitle && projectTitle.length > 0) {
+      return projectTitle[0].name;
+    } else {
+      return "Project title not found";
+    }
+  };
+
+  const currentProjectTitle = getCurrentProjectTitle();
+
   return (
     <>
       <HStack
@@ -17,11 +45,24 @@ const EditorPanel = () => {
         bgColor={theme.colors[colorMode].surfaceContainerLow}
         rounded={8}
       >
-        <Text ml={4}>Project title</Text>
+        <Tooltip label={"Change title"}>
+          {inputMode ? (
+            <HStack w="100%">
+              <Input placeholder={currentProjectTitle} />
+              <Spacer />
+              <CloseButton size="sm" onClick={() => setInputMode(!inputMode)} />
+            </HStack>
+          ) : (
+            <Text
+              ml={4}
+              cursor="pointer"
+              onClick={() => setInputMode(!inputMode)}
+            >
+              {currentProjectTitle}
+            </Text>
+          )}
+        </Tooltip>
         <Spacer />
-        <Button size="sm">Templates</Button>
-        <Button size="sm">Refine</Button>
-        <Button size="sm">Settings</Button>
       </HStack>
       <TextEditor />
     </>
