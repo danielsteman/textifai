cc9c527c848b975138a81eaa5295ea974e030cad
Daniel Steman <daniel-steman@live.nl>
2023-09-29 12:18:55 +0200

    :rocket: Pass secrets as build args

--- packages/web/src/features/Workspace/panels/EditorPanel.tsx
+++ packages/web/src/features/Workspace/panels/EditorPanel.tsx
@@ -1,5 +1,6 @@
-import { Button, HStack, Spacer, Text } from "@chakra-ui/react";
+import { Button, HStack, Spacer, Text, useColorMode } from "@chakra-ui/react";
 import TextEditor from "../../TextEditor/TextEditor";
+import theme from "../../../app/themes/theme";
 
 export interface CustomTabPanelProps {
   openChatSupport: boolean;
@@ -7,9 +8,15 @@ export interface CustomTabPanelProps {
 }
 
 const EditorPanel = () => {
+  const { colorMode } = useColorMode();
   return (
     <>
-      <HStack mb={2}>
+      <HStack
+        mb={2}
+        p={2}
+        bgColor={theme.colors[colorMode].surfaceContainerLow}
+        rounded={8}
+      >
         <Text ml={4}>Project title</Text>
         <Spacer />
         <Button size="sm">Templates</Button>
