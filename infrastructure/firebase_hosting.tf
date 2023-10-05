84900ba9f9bfc6e962edb17f81095abbe543aa22
Daniel Steman <daniel-steman@live.nl>
2023-10-05 22:13:37 +0200

    :rocket: Add firebase hosting for documents service

--- infrastructure/firebase_hosting.tf
+++ infrastructure/firebase_hosting.tf
@@ -1,20 +1,27 @@
-# resource "google_firebase_hosting_site" "default" {
-#   provider = google-beta
-#   project  = google_project.default.project_id
-#   site_id = "site-with-channel"
-#   app_id = google_firebase_web_app.default.app_id
-# }
+resource "google_firebase_hosting_site" "default" {
+  provider = google-beta
+  project  = google_project.default.project_id
+  site_id = "${google_project.default.project_id}-site"
+  app_id = google_firebase_web_app.default.app_id
+}
 
-# resource "google_firebase_hosting_version" "documents" {
-#   provider = google-beta
-#   site_id  = google_firebase_hosting_site.default.site_id
-#   config {
-#     rewrites {
-#       glob = "/api/documents/**"
-#       run {
-#         service_id = google_cloud_run_v2_service.service["documents"].name
-#         region = google_cloud_run_v2_service.service["documents"].location
-#       }
-#     }
-#   }
-# }
+resource "google_firebase_hosting_version" "documents" {
+  provider = google-beta
+  site_id  = google_firebase_hosting_site.default.site_id
+  config {
+    rewrites {
+      glob = "/api/documents/**"
+      run {
+        service_id = google_cloud_run_v2_service.service["documents"].name
+        region = google_cloud_run_v2_service.service["documents"].location
+      }
+    }
+  }
+}
+
+resource "google_firebase_hosting_release" "default" {
+  provider     = google-beta
+  site_id      = google_firebase_hosting_site.default.site_id
+  version_name = google_firebase_hosting_version.documents.name
+  message      = "Cloud Run Integration with Documents service"
+}
