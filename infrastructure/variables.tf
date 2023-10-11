1bb450b3d5239bc1672878a1266cc9d5ebd0e01f
Daniel Steman <daniel-steman@live.nl>
2023-10-11 22:09:01 +0200

    test

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -62,11 +62,10 @@ variable "oauth_client_secret" {
 }
 
 variable "image_tag" {
-  type    = string
-  default = "6ff0e36c7a38cd543893161ca458697483fd0dd8"
+  type = string
 }
 
 locals {
   image_url_prefix = "${var.location}-docker.pkg.dev/${var.project_name}-${var.unique_identifier}/${var.artifact_registry_name}"
-  image_urls       = { for key, name in var.package_names : key => "${local.image_url_prefix}/${name}:${var.image_tag}" }
+  image_urls       = { for key, name in var.package_names : key => "${local.image_url_prefix}/${name}:${tostring(var.image_tag)}" }
 }
