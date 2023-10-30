44ab8b3219505628fb79cb0f81820838a10f7cc1
Daniel Steman <daniel-steman@live.nl>
2023-10-30 10:27:53 +0100

    styling payments page wip

--- packages/web/src/features/Checkout/Payment.tsx
+++ packages/web/src/features/Checkout/Payment.tsx
@@ -1,18 +1,29 @@
 import { useEffect, useState } from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckoutForm";
-import { loadStripe } from "@stripe/stripe-js";
+import { Appearance, loadStripe } from "@stripe/stripe-js";
 import { config } from "../../app/config/config";
+import theme from "../../app/themes/theme";
+import { useColorMode } from "@chakra-ui/react";
 
 const Payment = () => {
   const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
   const [clientSecret, setClientSecret] = useState("");
+  const { colorMode } = useColorMode();
+
+  const appearance: Appearance = {
+    theme: "stripe",
+    variables: {
+      colorPrimaryText: theme.colors[colorMode].onPrimary,
+      colorPrimary: theme.colors[colorMode].primary,
+      colorBackground: theme.colors[colorMode].surface,
+    },
+  };
 
   useEffect(() => {
     // Create PaymentIntent as soon as the page loads
     fetch(`${config.payments.url}/api/payments/create-payment-intent`)
       .then((res) => {
-        console.log(res);
         return res.json();
       })
       .then(({ clientSecret }: any) => setClientSecret(clientSecret));
@@ -22,7 +33,7 @@ const Payment = () => {
     <>
       <h1>Payment</h1>
       {clientSecret && stripePromise && (
-        <Elements stripe={stripePromise} options={{ clientSecret }}>
+        <Elements stripe={stripePromise} options={{ clientSecret, appearance }}>
           <CheckoutForm />
         </Elements>
       )}
