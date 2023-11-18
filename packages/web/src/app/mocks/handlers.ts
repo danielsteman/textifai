970ca0a4e3a8a92ee6c8facae3bbda0355c77cd0
Daniel Steman <daniel-steman@live.nl>
2023-11-18 17:36:40 +0100

    :art: Remove legacy meuk

--- packages/web/src/app/mocks/handlers.ts
+++
@@ -1,8 +0,0 @@
-import { rest } from "msw";
-import { config } from "../config/config";
-
-export const handlers = [
-  rest.post(`http://${config.chat.url}/api/chat/ask`, (_, res, ctx) => {
-    return res(ctx.delay(500), ctx.status(200), ctx.json({ answer: "hoi" }));
-  }),
-];
