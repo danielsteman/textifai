1a30eaf7f486f4c4ea2d3499021b8220e865d687
Daniel Steman <daniel-steman@live.nl>
2023-11-21 17:31:30 +0100

    add artifact registry to dev

---
+++ infrastructure/dev/gcp_artifact_registry.tf
@@ -0,0 +1,7 @@
+resource "google_artifact_registry_repository" "textifai" {
+  location      = var.location
+  repository_id = var.artifact_registry_name
+  description   = "Docker repository"
+  format        = "DOCKER"
+  project       = google_project.dev.project_id
+}
