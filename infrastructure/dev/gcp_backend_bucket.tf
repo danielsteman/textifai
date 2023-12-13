530420f0ee4300c2b58cd95f19ac1c03494f592a
Daniel Steman <daniel-steman@live.nl>
2023-12-13 13:23:53 +0100

    fix tf

--- infrastructure/dev/gcp_backend_bucket.tf
+++ infrastructure/dev/gcp_backend_bucket.tf
@@ -2,9 +2,11 @@ resource "google_compute_backend_bucket" "dev" {
   name        = "image-backend-bucket"
   bucket_name = google_storage_bucket.dev.name
   enable_cdn  = true
+  project     = google_project.dev.id
 }
 
 resource "google_storage_bucket" "dev" {
   name     = "image-store-bucket"
   location = "EUROPE-WEST4"
+  project  = google_project.dev.id
 }
