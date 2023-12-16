d75dca05aa675f9168002695ea0ff530210ba97e
Daniel Steman <daniel-steman@live.nl>
2023-12-16 13:15:44 +0100

    :bug: Fix

--- packages/web/src/app/routes/Upload.tsx
+++ packages/web/src/app/routes/Upload.tsx
@@ -2,16 +2,9 @@ import { Box } from "@chakra-ui/react";
 import UploadForm from "../../features/UploadForm/UploadForm";
 
 const Upload = () => {
-
-  // Define the onUploadComplete callback
-  const handleUploadComplete = () => {
-    console.log('Upload complete!');
-  };
-
   return (
     <Box textAlign="center" flex="1">
-      {/* Pass the onUploadComplete prop to UploadForm */}
-      <UploadForm onUploadComplete={handleUploadComplete} />
+      <UploadForm />
     </Box>
   );
 };
